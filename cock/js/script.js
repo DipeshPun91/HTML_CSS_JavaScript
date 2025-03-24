3dbf61d782a2554d0846db2c67af25e4fb2cfd94
DipeshPun91 <pundipesh091@gmail.com>
2025-03-24 21:07:26 +0545

    Eighteen commit

---
+++ cock/js/script.js
@@ -0,0 +1,46 @@
+const clockFace = document.querySelector("svg g.clock--face");
+for (let i = 0; i < 12; i++) {
+  const angle = 30 * (i + 1);
+  const text = document.createElementNS("http://www.w3.org/2000/svg", "text");
+
+  text.setAttribute(
+    "transform",
+    `rotate(${angle}) translate(34 0) rotate(${-angle})`
+  );
+
+  if (i + 1 === 6 || i + 1 === 9) {
+    text.setAttribute(
+      "transform",
+      `rotate(${angle}) translate(34 0) rotate(${-angle + 180})`
+    );
+  }
+
+  text.textContent = i + 1;
+  clockFace.appendChild(text);
+}
+
+function updateClock() {
+  const now = new Date();
+  const hours = now.getHours() % 12;
+  const minutes = now.getMinutes();
+  const seconds = now.getSeconds();
+  const milliseconds = now.getMilliseconds();
+
+  const smoothSeconds = seconds + milliseconds / 1000;
+  const smoothMinutes = minutes + smoothSeconds / 60;
+  const smoothHours = hours + smoothMinutes / 60;
+
+  document
+    .querySelector(".seconds")
+    .setAttribute("transform", `rotate(${smoothSeconds * 6})`);
+  document
+    .querySelector(".minutes")
+    .setAttribute("transform", `rotate(${smoothMinutes * 6})`);
+  document
+    .querySelector(".hours")
+    .setAttribute("transform", `rotate(${smoothHours * 30 - 15})`);
+
+  requestAnimationFrame(updateClock);
+}
+
+updateClock();
